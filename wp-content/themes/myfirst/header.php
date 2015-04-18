e98bcb4d461c3b3a37cc4171d6aba8be7a38ded9
Chuck Obaze <chuckobaze@gmail.com>
2015-04-18 18:52:51 -0400

    .

--- wp-content/themes/myfirst/header.php
+++ wp-content/themes/myfirst/header.php
@@ -9,7 +9,7 @@
   <head>
     <title>easydevtuts</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>"/>
     <?php wp_head();?>
   </head>
   <body>
