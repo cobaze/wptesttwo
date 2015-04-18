8f7ef4e833caab2a4a2e603c375a20543ce0675d
Chuck Obaze <chuckobaze@gmail.com>
2015-04-18 18:49:02 -0400

    removed header

--- wp-content/themes/myfirst/index.php
+++ wp-content/themes/myfirst/index.php
@@ -1,39 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title><?php wp_title(); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="style.css" />
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>"/>
-    <?php wp_head();?>
-  </head>
-  <body>
-	<div id="wrap">
-		<!-- header: main-nav -->
-		<header>
-			<div class="container">
-				<div class="row">
-					<div class="span12">
-						<div class="page-header">
-						  <h1><a href="#">easydevtuts</a> <small>web development tutorials</small></h1>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-		<div class="navbar">
-          <div class="navbar-inner">
-            <div class="container">
-              <ul class="nav">
-                <li><a href="#">home</a></li>
-                <li><a href="#">blog</a></li>
-                <li><a href="#">contact</a></li>
-                <li><a href="#">about</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-		<!-- end of header -->
 
 	<!-- main home content -->
 	<div id="main-content" class="container">
@@ -193,34 +157,3 @@
   </body>
 </html>
 
-<!-- <!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8" />
-	<title><?php wp_title(); ?></title>
-	<link rel="stylesheet" type="text/css" href="style.css" />
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>"/>
-</head>
-<body>
-	<div id="wrapper">
-		<div id="header">
-			<h1><?php bloginfo('name');?></h1>
-			<div id="nav">
-				<ul>
-					<?php wp_list_pages('title_li=');?>
-				</ul>
-			</div>
-		</div>
-
-		<div id='main'>
-			<div id="content">
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<h1><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-				<?php endwhile; endif; ?>
-
-			</div>
-		</div>
-	</div>
-</body>
-</html>-->
